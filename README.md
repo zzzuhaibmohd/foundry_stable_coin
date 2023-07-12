e1fadd5d7c4b53d5d68e7815011a886f41e3303f
ziion <zzzuhaibmohd@gmail.com>
2023-07-11 23:58:56 -0700

    DSC Logic

---
+++ README.md
@@ -0,0 +1,29 @@
+Types of Stable Coins
+
+1. Relative Stability
+
+a. Pegged/Anchored - Example : USDT, USDC, DAI
+b. Floating: RAI Stable Coin
+
+2. Stablity Method
+
+a. Governed - Minting/Burning via Centralized Parties - USDT.USDC,TUSD
+b. Algorithmic - Minting/Burning via Code - DAI, FRAX and UST
+
+
+3. Collateral Based
+
+a. Exogenous Collateral - Collateral outside the protocol - USDC, DAI
+b. Endogenous Collateral - Collateral inside the protocol - UST
+
+
+------------------------------------------------------------------------------------------------------
+
+
+THE DESIGN
+
+1. Relative (Anchroed or Pegged) to USD -> Chainlink Price and Set a function to exchange ETH & BTC
+2. Stability (Minting) : Algorithmic(Decentralized) -> People can only mint with Enough collateral
+3. Collateral: Exogenous(wETH or wBTC)
+
+
---
+++ src/DSCEngine.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.16;
+
+//import {OracleLib, AggregatorV3Interface} from "./libraries/OracleLib.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+
+/*
+ * @title DSCEngine
+ * @author Zuhaib Mohammed
+ *
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg at all times.
+ * This is a stablecoin with the properties:
+ * - Exogenously Collateralized
+ * - Dollar Pegged
+ * - Algorithmically Stable
+ *
+ * It is similar to DAI if DAI had no governance, no fees, and was backed by only WETH and WBTC.
+ *
+ * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
+ * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * @notice This contract is based on the MakerDAO DSS system
+ */
+contract DSCEngine {
+
+}
